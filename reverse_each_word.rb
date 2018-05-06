e56d27eb12ecdd67c4f69f790bdc6d402259d21b
lpault29@gmail.com Ly <lpault29@gmail.com>
2018-05-06 00:08:07 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,8 @@
 def reverse_each_word(string) 
-  string.split(" ")
+  array = string.split(" ")
+  reversed_array = []
+  array.each do |x|
+    reversed_array.push(x.reverse)
+  end 
+  reversed_array.join(" ")
 end 
