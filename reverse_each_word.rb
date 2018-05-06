ad50da09a578b8fb5e0ea0b5a509102620677bb7
lpault29@gmail.com Ly <lpault29@gmail.com>
2018-05-06 00:16:13 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,11 @@
+def reverse_each_word(string) 
+  array = string.split(" ")
+  reversed_array = []
+  array.each do |x|
+    reversed_array.push(x.reverse)
+  end 
+  reversed_array.collect do |x|
+    x.reverse
+  end 
+  reversed_array.join(" ")
+end 
